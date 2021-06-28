412c46e4aba87d10640f7e6242020280f8b9fa71
Omer kurttekin <o.kurttekin@hotmail.com>
2021-06-28 14:52:59 +0300

    Starting Engine & Renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,15 @@
 #include <SFML/Graphics.hpp>
 
+#include "engine.h"
+
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(600, 600), "Tebris");
 
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  Engine::Renderer renderer(window);
 
-  while (window.isOpen())
-    {
-      sf::Event event;
-      while (window.pollEvent(event))
-        {
-          if (event.type == sf::Event::Closed)
-            window.close();
-        }
-
-      window.clear();
-      window.draw(shape);
-      window.display();
-    }
+  renderer.setup();
+  renderer.loop();
 
   return 0;
 }
